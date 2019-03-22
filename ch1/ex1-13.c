3c430313b06b7af387ac31fcd38df08107362863
Daniel Robins <dan@Daniels-MacBook-Air.local>
2019-03-23 08:25:42 +1100

    start 13

---
+++ ch1/ex1-13.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int one, two, three, four, five;
+
+    int c;
+    
+    int charcount[10];
+
+    for(int i = 0; i<10; i++) {
+        charcount[i] = 0;
+        printf("%d",charcount[i]);
+    }
+
+    while((c = getchar()) != EOF) {
+             
+    }
+}
