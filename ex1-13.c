ca62643155ad8894d7514ccea0dc86f05ced0945
Daniel Robins <danr92@gmail.com>
2019-06-03 00:58:11 +1000

    ex1-13 working. TODO: mod to horizontal Please enter the commit message for your changes. Lines starting

--- ex1-13.c
+++ ex1-13.c
@@ -3,39 +3,54 @@
 #define IN 1 // inside a word
 #define OUT 0 //outside a word
 
-int main(void)
+// count the length of words and print a histogram of
+// word length frequeny
+int main (void)
 {
-    int c, state, cnt;
-    
-    cnt = 0;
-    
+    int c, state;
+
     state = OUT;
-    
-    int charcount[10];
 
-    for(int i = 0; i<10; i++) {
-        charcount[i] = 0;
-//         printf("%d",charcount[i]);
-    }
+    //initialize all elements to zero. (missing assignments get 0 by default)
+    int hist[50] = { 0 };
 
-    while ((c = getchar()) != EOF) {
-        if (c == ' ' || c == '\n' || c == '\t') {
-           if (state==IN) {
-               putchar('\n');
-               charcount[cnt]++;
-               cnt = 0;
-           }
-           state = OUT;
-        } else if (state == OUT) {
-            putchar(c);
+    int char_count = 0;
+
+    while ((c = getchar()) != EOF) 
+    {
+        if (c == ' ' || c == '\n' || c == '\t') 
+        {
+            if (state==IN) 
+            {
+                hist[char_count]++;
+                char_count = 0;  
+                //            putchar('\n');
+
+            }
+            state = OUT;
+        } 
+        else if (state == OUT) 
+        {
+            //          putchar(c);
             state = IN;
-            cnt++;
-        } else {
-            putchar(c);
-            cnt++;
+            char_count = 1;
+        } 
+        else 
+        {
+            //            putchar(c);
+            char_count++;
         }
     }
-    for(int i = 0; i<10; i++) {
-        printf("%d",charcount[i]);
+    for (int i = 0; i <50; i++) {
+        if (hist[i]>0)
+        {
+            printf("%02d:%03d ",i,hist[i]);
+            for (int j = 0; j<hist[i];j++)
+            {
+                printf("*");
+            }
+            printf("\n");
+        }
     }
 }
+
