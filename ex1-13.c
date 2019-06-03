fe36efebd15461091de4b09e45e9d3f14439778c
Daniel Robins <danr92@gmail.com>
2019-06-03 23:12:41 +1000

    make screen array for print horizontal

--- ex1-13.c
+++ ex1-13.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stdint.h>
 
 #define IN 1 // inside a word
 #define OUT 0 //outside a word
+#define WIDTH 60
+#define HEIGHT 50
 
 // count the length of words and print a histogram of
 // word length frequeny
@@ -41,16 +44,35 @@ int main (void)
             char_count++;
         }
     }
-    for (int i = 0; i <50; i++) {
-        if (hist[i]>0)
+    
+    uint8_t screen[HEIGHT][WIDTH] = { 0 }; 
+    
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        for (int j = 0; j < WIDTH; j++)
         {
-            printf("%02d:%03d ",i,hist[i]);
-            for (int j = 0; j<hist[i];j++)
-            {
-                printf("*");
-            }
-            printf("\n");
+            screen[i][j] = 32;
         }
     }
+
+    for (int i = 0; i < 50; i++) {
+        if (hist[i]>0)
+        {
+            //printf("%02d:%03d ",i,hist[i]);
+            for (int j = 0; j<hist[i];j++)
+            {
+                screen[i][j] = 35;
+            }
+        }
+    }
+    for (int j = WIDTH-1; j > -1;j--)
+    {
+        for (int i = 0; i < HEIGHT; i++)
+        {
+            printf("%c", screen[i][j]);
+        }
+        printf("\n");
+    }
+
 }
 
